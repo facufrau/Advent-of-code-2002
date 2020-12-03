9f7e281009a2eaa96678df885a1747a3e7852390
Facundo Frau <facufrau@gmail.com>
2020-12-03 14:27:45 -0300

    Finished part 1 day 3

--- aoc_day_3.py
+++ aoc_day_3.py
@@ -1,14 +1,25 @@
 # --- Day 3: Toboggan Trajectory ---
 # by Facundo Frau - Github facufrau
-import time
 
 # Part one.
-with open('day3_input.txt','r') as f:
-    lines = f.readlines()
+# Trees are marked '#' in lines, '.' for open squares.
+trees = 0
 
-    # Trees are marked '#' in lines, '.' for free space.
-    trees = 0
-    for line in lines:
-        print(len(lines))
-        print(line)
-        time.sleep(3)
+with open('day3_input.txt','r') as f:
+    lines = f.read().splitlines()
+
+row = 0  # Row for iterating over lines
+block = 0  # Row for iterating along each line.
+
+while row < len(lines) - 1:
+    row += 1
+    block += 3
+    if block > 30:
+        block = block % 31
+        if lines[row][block] == '#':
+            trees += 1
+    else:
+        if lines[row][block] == '#':
+            trees += 1
+
+print(f"Part 1 answer - N° Trees: {trees}")
