d8308136429c3a5c87f87a8766f8f467eaebe517
Facundo Frau <facufrau@gmail.com>
2020-12-05 12:19:42 -0300

    Day 4 - Part 1 finished.

--- aoc_day_4.py
+++ aoc_day_4.py
@@ -17,4 +17,53 @@ with open('day4_input.txt', 'r') as f:
             new_line = ""
         else:
             new_line += line.replace("\n", " ")
-    print(len(passports[1]))
+            
+# Part one
+valid_passports = 0
+for value in passports.values():
+	if len(value) == 8:
+		valid_passports += 1
+	elif len(value) == 7 and ('cid' not in value):
+		valid_passports += 1
+
+print(f"Part 1 Answer - Valid passports: {valid_passports}")
+	
+# Part two
+def valid_byr(passport):
+	"""Validates byr -> 4 digits between 1920 and 2002."""
+	if 'byr' in passport:
+		if len(passport['byr']) == 4 and (1920 <=int(passport['byr']) <= 2002):
+			return True
+		else:
+			return False
+	else:
+		return False
+
+def valid_iyr(passport):
+	"""Validates iyr -> 4 digits between 2010 and 2020."""
+	if 'iyr' in passport:
+		if len(passport['iyr']) == 4 and (2010 <=int(passport['iyr']) <= 2020):
+			return True
+		else:
+			return False
+	else:
+		return False
+		
+def valid_eyr(passport):
+	"""Validates eyr -> 4 digits between 1920 and 2002."""
+	if 'eyr' in passport:
+		if len(passport['eyr']) == 4 and (2020 <=int(passport['eyr']) <= 2030):
+			return True
+		else:
+			return False
+	else:
+		return False
+	
+def valid_hgt(passport):
+	""" A number followed by either cm or in:
+		If cm, the number must be at least 150 and at most 193.
+		If in, the number must be at least 59 and at most 76.
+		(\d+)([a-z]{2}) REGEX
+		https://stackoverflow.com/questions/11592261/check-if-a-string-is-hexadecimal
+	"""
+	
