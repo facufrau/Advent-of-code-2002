c8b1778c56c0fba3a94fbdfcab55f23c2bc74faa
Facundo Frau <facufrau@gmail.com>
2020-12-13 12:13:52 -0300

    Completed file of day 10.

--- aoc_day_10.py
+++ aoc_day_10.py
@@ -1,8 +1,10 @@
 # --- Day 10: Adapter Array ---
 # by Facundo Frau - Github facufrau
+from math import prod
 
 with open('day10_input.txt') as f:
-    adapters = [int(x) for x in f.read().splitlines()].sort()
+    adapters = [int(x) for x in f.read().splitlines()]
+    adapters.sort()
     
 # Part one.
 diff_3 = 1
@@ -17,3 +19,28 @@ for i in range(len(adapters)):
         elif delta == 3:
             diff_3 += 1
 print(f"Part 1 answer - {diff_1 * diff_3}")
+
+# Part two.
+adapters.insert(0,0)
+adapters.insert(-1, max(adapters) + 3)
+adapters.sort()
+    
+diffs = [adapters[x]-adapters[x-1] for x in range(1, len(adapters))]
+
+sub_comb = []
+elements = 0
+for i in range(len(diffs)):
+    if diffs[i] == 3:
+        if elements == 4:
+            sub_comb.append(7)
+        elif elements == 3:
+            sub_comb.append(4)
+        elif elements == 2:
+            sub_comb.append(2)
+        else:
+            sub_comb.append(1)
+        elements = 0
+    else:
+        elements += 1
+total_comb = prod(sub_comb)
+print(f"Part 2 answer - Combinations = {total_comb}")
