7f725578b13090792d57a427daa3ab29b1bce60e
Facundo Frau <facufrau@gmail.com>
2020-12-21 09:12:20 -0300

    Completed part 1 - day 13.

---
+++ aoc_day_13.py
@@ -0,0 +1,29 @@
+# --- Day 13: Shuttle Search ---
+# by Facundo Frau - Github facufrau
+from math import gcd
+import itertools
+# Part one.
+with open('day13_input.txt') as f:
+    timestamp = int(f.readline())
+    buses = [int(x) for x in f.readline().split(',') if x != 'x']
+buses_freq = {}
+for bus in buses:
+    next_freq = ((timestamp // bus) + 1) * bus -  timestamp
+    buses_freq[bus] = next_freq
+bus_id = min(buses_freq, key=buses_freq.get)
+answer = bus_id * buses_freq[bus_id]
+print(f"Part one - ID earliest bus times minutes waiting: {answer}")
+
+# Part two.
+with open('day13_input.txt') as f:
+    buses = [x for x in f.readlines()[1].split(',')]
+    buses = [int(x) if x != 'x' else x for x in buses ]
+    print(buses)
+schedule = {}
+for bus in buses:
+    if bus == 'x':
+        continue
+    else:
+        schedule[bus] = buses.index(bus)
+print(schedule)
+print(gcd(17,13))
