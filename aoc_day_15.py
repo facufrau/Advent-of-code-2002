0203e3fdcb3fa7af87275fb21e534c9b73d3db4a
Facundo Frau <facufrau@gmail.com>
2020-12-23 10:11:19 -0300

    Day 15 completed.

--- aoc_day_15.py
+++ aoc_day_15.py
@@ -1,21 +1,28 @@
 # --- Day 15: Rambunctious Recitation ---
 # by Facundo Frau - Github facufrau
-
 starting = [13,16,0,12,15,1]
-numbers = []
-turn = 1
-for n in starting:
-    numbers.append(n)
-    turn += 1
 
-while turn <= 2020:
-    n = numbers[-1]
-    if n not in numbers[:-1]:
-        numbers.append(0)
+def memory_game(start_numbers, goal):
+    last_turn = {}
+    turn = 1
+    for n in start_numbers:
+        last_turn[n] = turn
         turn += 1
-    else:
-        indexes = [(i + 1) for i, x in enumerate(numbers) if x == n]
-        next = (turn - 1) - indexes[-2]
-        numbers.append(next)
+    n = start_numbers[-1]
+
+    while turn <= goal:
+        if n not in last_turn:
+            last_turn[n] = turn - 1
+            n = 0
+        else:
+            next = (turn - 1) - last_turn[n]
+            last_turn[n] = turn - 1
+            n = next
         turn += 1
-print(f"Part one answer - {numbers[-1]}")
+    return n
+
+part_one = memory_game(starting, 2020)
+print(f"Part one answer - {part_one}")
+
+part_two = memory_game(starting, 30000000)
+print(f"Part two answer - {part_two}")
