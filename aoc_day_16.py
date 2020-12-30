540ef7a19ea0fa43f8966a9dfb347f9ce99c512a
Facundo Frau <facufrau@gmail.com>
2020-12-30 09:11:34 -0300

    Completed day 16 part 1

--- aoc_day_16.py
+++ aoc_day_16.py
@@ -17,7 +17,7 @@ with open('day16_input.txt') as f:
             else:
                 line = [int(x) if i > 0 else x for i, x in enumerate(line)]
                 rules[line[0]] = tuple(line[1:])
-    #print(rules)
+    print(rules)
 
     # Store my ticket as a list of numbers.
     my_ticket = lines[lines.index('your ticket:') + 1].split(',')
@@ -33,13 +33,42 @@ with open('day16_input.txt') as f:
 
 # Part one
 error_rate = 0
+valid_tickets = []  # Used in part two.
 for ticket in nearby_tickets:
+    is_valid = True
     for field in ticket:
         valid = 0
         for value in rules.values():
             if (value[0] <= field <= value[1]) or (value[2] <= field <= value[3]):
                 valid += 1
         if valid == 0:
-            error_rate += field  
+            error_rate += field
+            is_valid = False
+    if is_valid:  
+        valid_tickets.append(ticket)
 print(f"Part 1 answer - Scanning error rate: {error_rate}")
-                 
+print(valid_tickets)
+
+# Part two
+total = len(valid_tickets)
+field_order = {}
+
+columns = []
+for i in range(total):
+    col = []
+    for ticket in valid_tickets:
+        col.append(ticket[i])
+    columns.append(col)
+
+print(columns)
+for key, value in rules.items():
+    for item in columns:
+        counter = 0
+        for i in item:
+            if (value[0] <= i <= value[1]) or (value[2] <= i <= value[3]):
+                counter += 1
+        if counter == total:
+            field_order[key] = columns.index(item)
+print(field_order)
+
+        
