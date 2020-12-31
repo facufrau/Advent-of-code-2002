400d364f6a9bb53be0fd92587e1b5b4a3ae1f50d
Facundo Frau <facufrau@gmail.com>
2020-12-30 22:01:15 -0300

    Completed day 13.

--- aoc_day_13.py
+++ aoc_day_13.py
@@ -1,7 +1,7 @@
 # --- Day 13: Shuttle Search ---
 # by Facundo Frau - Github facufrau
-from math import gcd
-import itertools
+from math import prod
+
 # Part one.
 with open('day13_input.txt') as f:
     timestamp = int(f.readline())
@@ -18,12 +18,41 @@ print(f"Part one - ID earliest bus times minutes waiting: {answer}")
 with open('day13_input.txt') as f:
     buses = [x for x in f.readlines()[1].split(',')]
     buses = [int(x) if x != 'x' else x for x in buses ]
-    print(buses)
 schedule = {}
 for bus in buses:
     if bus == 'x':
         continue
     else:
         schedule[bus] = buses.index(bus)
-print(schedule)
-#TODO - COMPLETE PART 2
+
+# Help and idea for solving the CRT taken from this video in youtube.
+# https://www.youtube.com/watch?v=zIFehsBHB8o - Maths with jay channel.
+
+def solve_system(buses_schedule):
+    # Initialize data structures for table.
+    N_value = prod(buses_schedule.keys())
+    b_list = []
+    Ni_list = []
+    xi_list = []
+    # Calculate bi elements
+    for k, v in buses_schedule.items():
+        if v == 0:
+            b_list.append(v)
+        else:
+            b_list.append(k-v)
+    # Calculate Ni elements
+    for k in buses_schedule.keys():
+        Ni_list.append(N_value // k)
+    # Calculate xi elements
+    for i,n in enumerate(Ni_list):
+        mod = N_value // Ni_list[i]
+        xi_list.append(pow(n, -1, mod))
+    # Calculate final products
+    final_values = []
+    for i in range(len(b_list)):
+        prod_b_n_i = b_list[i] *  Ni_list[i] * xi_list[i]
+        final_values.append(prod_b_n_i)
+    return sum(final_values) % N_value
+   
+answer_2 = solve_system(schedule)
+print(f"Part two - Timestamp T for requirements: {answer_2}")
