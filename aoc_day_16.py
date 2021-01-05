ac9097e8299afb285c621e8faf1baff0e438a725
Facundo Frau <facufrau@gmail.com>
2021-01-05 20:12:37 -0300

    Completed day 16

--- aoc_day_16.py
+++ aoc_day_16.py
@@ -17,7 +17,6 @@ with open('day16_input.txt') as f:
             else:
                 line = [int(x) if i > 0 else x for i, x in enumerate(line)]
                 rules[line[0]] = tuple(line[1:])
-    print(rules)
 
     # Store my ticket as a list of numbers.
     my_ticket = lines[lines.index('your ticket:') + 1].split(',')
@@ -29,7 +28,6 @@ with open('day16_input.txt') as f:
     for line in lines[separator:]:
         ticket = [int(x) for x in line.split(',')]
         nearby_tickets.append(ticket)
-#print(nearby_tickets)
 
 # Part one
 error_rate = 0
@@ -47,12 +45,9 @@ for ticket in nearby_tickets:
     if is_valid:  
         valid_tickets.append(ticket)
 print(f"Part 1 answer - Scanning error rate: {error_rate}")
-print(valid_tickets)
 
 # Part two
-total = len(valid_tickets)
-field_order = {}
-
+total = len(valid_tickets[0])
 columns = []
 for i in range(total):
     col = []
@@ -60,7 +55,9 @@ for i in range(total):
         col.append(ticket[i])
     columns.append(col)
 
-print(columns)
+# Dict for storing which columns could match which fields.
+valid_columns = {}
+total = len(columns[0])
 for key, value in rules.items():
     for item in columns:
         counter = 0
@@ -68,7 +65,31 @@ for key, value in rules.items():
             if (value[0] <= i <= value[1]) or (value[2] <= i <= value[3]):
                 counter += 1
         if counter == total:
-            field_order[key] = columns.index(item)
-print(field_order)
+            if key not in valid_columns:
+                valid_columns[key] = [columns.index(item)]
+            else:
+                valid_columns[key].append(columns.index(item))
 
-        
+# Use total number of rules.
+total_rules = len(rules)
+
+# Function that removes repeating values in valid columns dict.
+# First starts with the columns that have only 1 field matching.
+# Then start removing those values and repeat until all have 1 valid field.
+def remove_values(dict_rules, val):
+    for k, v in dict_rules.items():
+        if len(v) != 1:
+            if val in dict_rules[k]:
+                dict_rules[k].remove(val)
+
+while not all(len(value) == 1 for value in valid_columns.values()):
+    for fields, columns in valid_columns.items():
+        if len(columns) == 1:
+            remove_values(valid_columns, columns[0])
+
+# Get final answer. Starting value = 1 for multiplying.
+answer_2 = 1
+for k, v in valid_columns.items():
+    if k.startswith('departure'):
+        answer_2 *= my_ticket[v[0]]
+print(f"Part 2 answer - 'departure' fields mult: {answer_2}")
